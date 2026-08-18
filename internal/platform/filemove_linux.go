a2fb58d70580f61c9068b493fd9ebe529b7cbee7
Brendigo <info@brendigo.com>
2026-08-19 01:32:31 +0200

    Use atomic Linux RENAME_NOREPLACE

---
+++ internal/platform/filemove_linux.go
@@ -0,0 +1,35 @@
+//go:build linux
+
+package platform
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// RenameNoReplace uses renameat2(RENAME_NOREPLACE), which asks the kernel to
+// reject an existing destination atomically instead of checking the path first.
+func RenameNoReplace(src, dst string) error {
+	s, err := syscall.BytePtrFromString(src)
+	if err != nil {
+		return err
+	}
+	d, err := syscall.BytePtrFromString(dst)
+	if err != nil {
+		return err
+	}
+	const (
+		atFDCWD         = -100
+		renameNoReplace = 1
+	)
+	_, _, errno := syscall.Syscall6(
+		syscall.SYS_RENAMEAT2,
+		uintptr(int(atFDCWD)), uintptr(unsafe.Pointer(s)),
+		uintptr(int(atFDCWD)), uintptr(unsafe.Pointer(d)),
+		uintptr(renameNoReplace), 0,
+	)
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
