6827898e9c58836deab4a20c0b0efcf34a397932
Brendigo <info@brendigo.com>
2026-08-18 23:26:20 +0200

    Primijeni client mode na desktop protokole

--- internal/desktop/mode.go
+++ internal/desktop/mode.go
@@ -12,12 +12,12 @@ var (
 )
 
 // SetClientMode se poziva jednom tijekom startupa prije stvaranja UI-a.
-// Lock ostaje namjerno jer paket ima i testne/terminalne ulaze koji se mogu
-// pokretati paralelno u istom test procesu.
+// Mode određuje dostupne protokole i korisnički identitet odvojenog klijenta.
 func SetClientMode(next clientmode.Mode) {
 	modeMu.Lock()
 	mode = next
 	modeMu.Unlock()
+	configureProtocolMode(next)
 }
 
 func clientMode() clientmode.Mode {
