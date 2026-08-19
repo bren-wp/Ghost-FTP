4205a701df4db0d4502c82066cff37ad5d014699
Brendigo <info@brendigo.com>
2026-08-20 01:00:22 +0200

    Keep Unix cancellation bound to process groups

--- internal/remote/process_other.go
+++ internal/remote/process_other.go
@@ -19,45 +19,23 @@ func configureToolCommand(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	originalCancel := cmd.Cancel
 	cmd.Cancel = func() error {
-		groupErr := killToolProcessGroup(cmd)
-		if groupErr == nil {
-			return nil
+		if cmd.Process != nil && cmd.Process.Pid > 0 {
+			err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+			if err == nil || errors.Is(err, syscall.ESRCH) {
+				return nil
+			}
+			if originalCancel == nil {
+				return err
+			}
+			directErr := originalCancel()
+			if directErr == nil || errors.Is(directErr, os.ErrProcessDone) {
+				return nil
+			}
+			return errors.Join(err, directErr)
 		}
-		if originalCancel == nil {
-			return groupErr
+		if originalCancel != nil {
+			return originalCancel()
 		}
-		directErr := originalCancel()
-		if directErr == nil || errors.Is(directErr, os.ErrProcessDone) {
-			return nil
-		}
-		return errors.Join(groupErr, directErr)
-	}
-}
-
-func killToolProcessGroup(cmd *exec.Cmd) error {
-	if cmd == nil || cmd.Process == nil || cmd.Process.Pid <= 0 {
 		return os.ErrProcessDone
 	}
-	err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-	if errors.Is(err, syscall.ESRCH) {
-		return os.ErrProcessDone
-	}
-	return err
-}
-
-func runToolCommand(cmd *exec.Cmd) error {
-	if cmd == nil {
-		return errors.New("vanjski proces nije postavljen")
-	}
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	waitErr := cmd.Wait()
-	// A helper can outlive a parent that exits normally. Kill any remaining
-	// members before returning so secrets/prompts cannot survive the operation.
-	cleanupErr := killToolProcessGroup(cmd)
-	if errors.Is(cleanupErr, os.ErrProcessDone) {
-		cleanupErr = nil
-	}
-	return errors.Join(waitErr, cleanupErr)
 }
