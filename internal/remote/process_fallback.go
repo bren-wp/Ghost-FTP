bbad76c58b03328ad75096ec0df25257a34b8ab5
Brendigo <info@brendigo.com>
2026-08-20 01:00:33 +0200

    Keep fallback process configuration minimal

--- internal/remote/process_fallback.go
+++ internal/remote/process_fallback.go
@@ -2,16 +2,6 @@
 
 package remote
 
-import (
-	"errors"
-	"os/exec"
-)
+import "os/exec"
 
 func configureToolCommand(cmd *exec.Cmd) {}
-
-func runToolCommand(cmd *exec.Cmd) error {
-	if cmd == nil {
-		return errors.New("vanjski proces nije postavljen")
-	}
-	return cmd.Run()
-}
