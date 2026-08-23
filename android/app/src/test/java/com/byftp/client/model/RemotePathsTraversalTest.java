1bda606e5928c4c16cf9af39bfa4d66563b6ca2e
Brendigo <info@brendigo.com>
2026-08-24 01:40:20 +0200

    Expand Android remote path traversal regressions

--- android/app/src/test/java/com/byftp/client/model/RemotePathsTraversalTest.java
+++ android/app/src/test/java/com/byftp/client/model/RemotePathsTraversalTest.java
@@ -4,13 +4,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RemotePathsTraversalTest {
-    @Test public void childRejectsTraversalNames() {
+    @Test public void childRejectsTraversalAndNoncanonicalNames() {
         assertThrows(IllegalArgumentException.class, () -> RemotePaths.child("/public_html", "../secret"));
         assertThrows(IllegalArgumentException.class, () -> RemotePaths.child("/public_html", "."));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.child("/public_html", " name"));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.child("/public_html", "name "));
+    }
+
+    @Test public void directoryRejectsTraversalAndSeparatorRewrites() {
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.normalizeDirectory("/public_html/../etc"));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.normalizeDirectory("/public_html/./assets"));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.normalizeDirectory("/public_html//assets"));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.normalizeDirectory("/public_html\\assets"));
+        assertThrows(IllegalArgumentException.class, () -> RemotePaths.normalizeDirectory("public_html"));
     }
 
     @Test public void parentNeverEscapesRoot() {
         assertEquals("/", RemotePaths.parent("/"));
         assertEquals("/", RemotePaths.parent("/one"));
+        assertEquals("/one", RemotePaths.parent("/one/two/"));
     }
 }
