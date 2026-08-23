e80aeb8c0b9547e222eacae9a1e5bb20c934493e
Brendigo <info@brendigo.com>
2026-08-23 22:38:33 +0200

    refactor: make ByFTP branding vendor-neutral

--- internal/brand/brand.go
+++ internal/brand/brand.go
@@ -2,8 +2,8 @@ package brand
 
 const (
 	ProductName = "ByFTP"
-	ProductFull = "ByFTP klijent"
-	Company     = "Brendigo"
-	Website     = "brendigo.com"
-	Support     = "info@brendigo.com"
+	ProductFull = "ByFTP file transfer client"
+	Company     = "ByFTP"
+	Website     = "https://github.com/bren-wp/by-ftp"
+	Support     = "https://github.com/bren-wp/by-ftp/issues"
 )
