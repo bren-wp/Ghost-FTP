3d9b3ea5f96be2d58734486b245848098ea5253f
Brendigo <info@brendigo.com>
2026-09-05 23:33:49 +0200

    Fix Windows action-state block after reference toolbar integration

--- internal/desktop/action_state_windows.go
+++ internal/desktop/action_state_windows.go
@@ -56,6 +56,7 @@ func (a *app) updateActionControls() {
 			if index >= 0 && index < len(a.remoteItems) && !a.remoteItems[index].IsSymlink {
 				chmodSelected++
 			}
+		}
 	}
 	setControlEnabled(a.remoteChmod, remoteReady && chmodSelected > 0)
 
