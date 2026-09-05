f26befd23fcf2bd1e1b28de83b6cde44bc530acd
Brendigo <info@brendigo.com>
2026-09-05 21:48:02 +0200

    Keep refined dual-pane layout across resize and protocol changes

--- internal/desktop/windows.go
+++ internal/desktop/windows.go
@@ -204,6 +204,7 @@ func wndProc(hwnd uintptr, message uint32, wParam, lParam uintptr) uintptr {
 		w := int(lParam & 0xffff)
 		h := int((lParam >> 16) & 0xffff)
 		a.layout(w, h)
+		a.refineWorkspaceLayout()
 		return 0
 	case wmDpiChanged:
 		newDPI := uint32((wParam >> 16) & 0xffff)
@@ -222,6 +223,7 @@ func wndProc(hwnd uintptr, message uint32, wParam, lParam uintptr) uintptr {
 		if id == idProtocol && notify == cbnSelChange {
 			a.syncDefaultPort()
 			a.updateProtocolControls()
+			a.refineWorkspaceLayout()
 			return 0
 		}
 		if id == idProfiles && notify == cbnSelChange {
@@ -230,6 +232,7 @@ func wndProc(hwnd uintptr, message uint32, wParam, lParam uintptr) uintptr {
 		}
 		if id == idLanguage && notify == cbnSelChange {
 			a.changeLanguageFromUI()
+			a.refineWorkspaceLayout()
 			return 0
 		}
 		if notify == bnClicked {
