b79361c86d8bed2354cad8e62c9bc88cfd020438
Brendigo <info@brendigo.com>
2026-09-06 02:17:35 +0200

    Remove retired Web surface and 0.1.1 one-shot tooling

--- GhostFTP WEB/app/Views/auth-brand.php
+++
@@ -1,4 +0,0 @@
-<div class="auth-brand-lockup">
-    <img src="<?= GhostFTP_e(GhostFTP_asset('images/logo.svg')) ?>" class="auth-logo" alt="Ghost FTP">
-    <span class="auth-version">v<?= GhostFTP_e(GhostFTP_VERSION) ?></span>
-</div>
