36ce528f1649b88e1c63e705ad08fe12a9d4b191
Brendigo <info@brendigo.com>
2026-09-07 02:16:03 +0200

    Avoid clipped Site Manager password cue

--- internal/desktop/site_manager_windows.go
+++ internal/desktop/site_manager_windows.go
@@ -522,7 +522,7 @@ func (state *siteManagerState) createControls(hinst uintptr) error {
 	limitEdit(state.remotePath, 4096)
 	limitEdit(state.keyPath, 32767)
 	limitEdit(state.passphrase, 8192)
-	cue(state.password, parent.tr("cue.password"))
+	cue(state.password, parent.tr("terminal.password"))
 	cue(state.passphrase, parent.tr("cue.passphrase"))
 	return nil
 }
@@ -621,6 +621,5 @@ func (a *app) openSiteManager() {
 				break
 			}
 		}
-	}
 	a.connectNow()
 }
