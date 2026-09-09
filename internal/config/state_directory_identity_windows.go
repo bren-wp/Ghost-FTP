7ce1d24d32402aad6bf9d5c234a8f1c6b3c17cd2
Brendigo <info@brendigo.com>
2026-09-09 03:52:04 +0200

    Define Windows state directory identity

---
+++ internal/config/state_directory_identity_windows.go
@@ -0,0 +1,9 @@
+//go:build windows
+
+package config
+
+import "os"
+
+func sameStateDirectoryIdentity(before, after os.FileInfo) bool {
+	return before != nil && after != nil && os.SameFile(before, after)
+}
