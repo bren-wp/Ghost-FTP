3b689e7ada6a3c65843c0af6545b75b47bae4575
Brendigo <info@brendigo.com>
2026-09-09 20:33:45 +0200

    Security: bind integrated uninstall to executable digest

--- cmd/installer/uninstall_registration_windows.go
+++ cmd/installer/uninstall_registration_windows.go
@@ -10,7 +10,14 @@ import (
 	"github.com/bren-wp/Ghost-FTP/internal/platform"
 )
 
+const installedExecutableDigestValue = "InstalledExecutableSHA256"
+
 func registerIntegratedUninstall(appPath, currentVersion string) error {
+	digest, err := platform.VerifiedRegularFileSHA256(appPath)
+	if err != nil {
+		return fmt.Errorf("installed executable ownership digest could not be verified: %w", err)
+	}
+
 	quoted := fmt.Sprintf("\"%s\" --uninstall", appPath)
 	values := []struct {
 		name  string
@@ -22,6 +29,7 @@ func registerIntegratedUninstall(appPath, currentVersion string) error {
 		{"InstallLocation", filepath.Dir(appPath)},
 		{"DisplayIcon", appPath + ",0"},
 		{"UninstallString", quoted},
+		{installedExecutableDigestValue, digest},
 		{"URLInfoAbout", brand.Website},
 	}
 	for _, item := range values {
