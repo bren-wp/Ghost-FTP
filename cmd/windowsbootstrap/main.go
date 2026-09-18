6371440c3c61c41f639cbbde1a2c11521192ff51
Brendigo <info@brendigo.com>
2026-09-18 09:25:43 +0200

    release: bind cmd/windowsbootstrap/main.go fallback to 0.0.8

--- cmd/windowsbootstrap/main.go
+++ cmd/windowsbootstrap/main.go
@@ -24,7 +24,7 @@ var payload embed.FS
 
 const maxEmbeddedExecutableSize = 256 << 20
 
-var version = "0.0.7"
+var version = "0.0.8"
 var role = "portable"
 
 func normalizedRole(value string) (string, error) {
