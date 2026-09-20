eb5e0663bbd7b7f5b00e8834a88b52b3263eec65
Brendigo <info@brendigo.com>
2026-09-20 02:21:15 +0200

    Update RC6 source reassembly checksum

--- bootstrap/reassemble.sh
+++ bootstrap/reassemble.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cat source.part.*.b64 | base64 -d > Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE-TEXT.tar.xz
-echo "8291c7a62d446be3728a4e805974a5aa78f00e96eb07512530ac0cece7f4cb0e  Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE-TEXT.tar.xz" | sha256sum -c -
-tar -xJf Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE-TEXT.tar.xz
+cat source.part.*.b64 | base64 -d > Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz
+echo "65fa0cab91803ee82b418a2cc17371c241f750567b550747cfb5ffb79059d098  Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz" | sha256sum -c -
+tar -xJf Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz
