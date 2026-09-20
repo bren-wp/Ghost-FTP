3ce40c76c39c75c70be51d7714a5ee909bbe106f
Brendigo <info@brendigo.com>
2026-09-21 00:40:52 +0200

    Advance Ghost FTP release metadata to RC13

--- ghostftp-desktop/src/lib/release.ts
+++ ghostftp-desktop/src/lib/release.ts
@@ -1,8 +1,8 @@
 export const PRODUCT_NAME = "Ghost FTP";
-export const PRODUCT_VERSION = "2.1.1-rc.12";
-export const PRODUCT_VERSION_DISPLAY = "2.1.1 RC12";
-export const PRODUCT_VERSION_BADGE = "RC12";
-export const PRODUCT_BUILD = "2026.09.20.12";
-export const PRODUCT_RELEASE_DATE = "20 September 2026";
+export const PRODUCT_VERSION = "2.1.1-rc.13";
+export const PRODUCT_VERSION_DISPLAY = "2.1.1 RC13";
+export const PRODUCT_VERSION_BADGE = "RC13";
+export const PRODUCT_BUILD = "2026.09.21.13";
+export const PRODUCT_RELEASE_DATE = "21 September 2026";
 export const PRODUCT_SITE = "https://ghostftp.com";
 export const UPDATE_ENDPOINT = `${PRODUCT_SITE}/updates/latest.json`;
