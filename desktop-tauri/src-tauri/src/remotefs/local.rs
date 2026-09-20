42cf5e9aacd2bed7e05f70700ccdb848bf488f3c
Brendigo <info@brendigo.com>
2026-09-20 06:13:34 +0200

    Fix LocalFs platform import syntax

--- desktop-tauri/src-tauri/src/remotefs/local.rs
+++ desktop-tauri/src-tauri/src/remotefs/local.rs
@@ -1,5 +1,7 @@
 use super::{Capabilities, ChangeSignal, DirEntry, FileKind, RemoteFs};
-use anyhow::Context;\n#[cfg(not(unix))]\nuse anyhow::anyhow;
+use anyhow::Context;
+#[cfg(not(unix))]
+use anyhow::anyhow;
 use async_trait::async_trait;
 use std::path::Path;
 
