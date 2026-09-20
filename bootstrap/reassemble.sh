7e52a53c4f197f30aa61079219b0288c8c60f591
Brendigo <info@brendigo.com>
2026-09-20 02:51:26 +0200

    Update bootstrap reassembly for RC7

--- bootstrap/reassemble.sh
+++ bootstrap/reassemble.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cat source.part.*.b64 | base64 -d > Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz
-echo "65fa0cab91803ee82b418a2cc17371c241f750567b550747cfb5ffb79059d098  Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz" | sha256sum -c -
-tar -xJf Ghost-FTP-v2.1.1-RC6-GITHUB-SOURCE.tar.xz
+cat source.part.*.b64 | base64 -d > Ghost-FTP-v2.1.1-RC7-GITHUB-SOURCE.tar.xz
+echo "146c5d0a467c3789c9bb87273879950ed219b1f66616357cbf5e4aed127d8ac0  Ghost-FTP-v2.1.1-RC7-GITHUB-SOURCE.tar.xz" | sha256sum -c -
+tar -xJf Ghost-FTP-v2.1.1-RC7-GITHUB-SOURCE.tar.xz
