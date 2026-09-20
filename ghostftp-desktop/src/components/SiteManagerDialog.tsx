00f6494473c04c5f60f972db4d1536852898ee06
Brendigo <info@brendigo.com>
2026-09-21 00:31:19 +0200

    Move primary Ghost FTP views into the single application shell

--- ghostftp-desktop/src/components/SiteManagerDialog.tsx
+++ ghostftp-desktop/src/components/SiteManagerDialog.tsx
@@ -248,9 +248,8 @@ export function SiteManagerDialog({ onClose }: Props) {
 
   return (
     <div
-      className="ghost-standalone-view fixed inset-0 z-modal bg-[#041425]"
-      role="dialog"
-      aria-modal="true"
+      className="ghost-workspace-view ghost-standalone-view bg-[#041425]"
+      role="region"
       aria-label="Site Manager"
     >
       <div
