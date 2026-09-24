2ec655aafb99f2ceaa8579f17e6ecd5c328cab9e
Brendigo <info@brendigo.com>
2026-09-24 23:42:10 +0200

    Bump Android release info to RC22

--- android/app/src/main/java/com/ghostftp/android/ReleaseInfo.kt
+++ android/app/src/main/java/com/ghostftp/android/ReleaseInfo.kt
@@ -3,10 +3,10 @@ package com.ghostftp.android
 object ReleaseInfo {
     const val PRODUCT_NAME = "Ghost FTP"
     const val BRAND = "Brendigo"
-    const val VERSION = "2.1.1-rc.21"
-    const val VERSION_DISPLAY = "2.1.1 RC21"
-    const val VERSION_BADGE = "RC21"
-    const val BUILD = "2026.09.24.21"
+    const val VERSION = "2.1.1-rc.22"
+    const val VERSION_DISPLAY = "2.1.1 RC22"
+    const val VERSION_BADGE = "RC22"
+    const val BUILD = "2026.09.24.22"
     const val RELEASE_DATE = "24 September 2026"
     const val WEBSITE = "https://ghostftp.com"
 }
