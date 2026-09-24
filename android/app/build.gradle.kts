fb8c613e2bebd565f34429bbab443cebe09c2b88
Brendigo <info@brendigo.com>
2026-09-24 23:42:15 +0200

    Bump Android Gradle version to RC22

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ghostftp.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 211021
-        versionName = "2.1.1-rc.21"
+        versionCode = 211022
+        versionName = "2.1.1-rc.22"
     }
 
     signingConfigs {
