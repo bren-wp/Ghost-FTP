d3ee3fe82164a99b18a9eede9ada68f4c497372a
Brendigo <info@brendigo.com>
2026-09-25 21:34:28 +0200

    Bump desktop release constants to RC23

--- ghostftp-desktop/src/lib/release.ts
+++ ghostftp-desktop/src/lib/release.ts
@@ -1,8 +1,8 @@
 export const PRODUCT_NAME = "Ghost FTP";
-export const PRODUCT_VERSION = "2.1.1-rc.22";
-export const PRODUCT_VERSION_DISPLAY = "2.1.1 RC22";
-export const PRODUCT_VERSION_BADGE = "RC22";
-export const PRODUCT_BUILD = "2026.09.24.22";
-export const PRODUCT_RELEASE_DATE = "24 September 2026";
+export const PRODUCT_VERSION = "2.1.1-rc.23";
+export const PRODUCT_VERSION_DISPLAY = "2.1.1 RC23";
+export const PRODUCT_VERSION_BADGE = "RC23";
+export const PRODUCT_BUILD = "2026.09.25.23";
+export const PRODUCT_RELEASE_DATE = "25 September 2026";
 export const PRODUCT_SITE = "https://ghostftp.com";
 export const UPDATE_ENDPOINT = `${PRODUCT_SITE}/updates/latest.json`;
